ce71058319b7535f8dff8f1099deae5a99e87771
ShinChangHak <csg1353@naver.com>
2023-05-23 17:54:34 +0900

    [ADD]스와이퍼추가, DB, 버그수정

--- FrontEnd/vue_0517/src/views/HomeView.vue
+++ FrontEnd/vue_0517/src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
+    <homeSwiperComp></homeSwiperComp>
     <homeSearchComp> </homeSearchComp>
   </div>
 </template>
 
 <script>
+import homeSwiperComp from "@/components/search/homeSwiperComp.vue";
 import homeSearchComp from "@/components/search/homeSearchComp.vue";
 export default {
   name: "HomeView",
-  components: { homeSearchComp },
+  components: { homeSearchComp, homeSwiperComp },
 };
 </script>
 <style scoped></style>
